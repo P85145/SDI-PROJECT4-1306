110991cfa6cbeb8ee69a5add242e9f58dd50b354
P85145 <CeCe@Cerelias-MacBook-Pro.local>
2013-06-27 19:19:56 -0400

    Library

---
+++ williams_cerelia_assignment4_1306.js
@@ -0,0 +1,74 @@
+//alert("JavaScript works!");
+// Cerelia Williams
+// SDI 1306
+// Project 4
+// 6/27/13
+// Library of Functions
+
+
+
+var myLibrary = function(){
+
+	// FUNCTION
+	
+	
+	
+	// FUNCTION
+	
+	
+	
+	// FUNCTION
+	
+	
+	
+	// FUNCTION
+	
+	
+	
+	// FUNCTION
+	
+	
+	
+	// FUNCTION
+
+
+
+
+
+
+
+	// Objects
+	return {
+		" ": ,
+		
+	}
+	
+}
+
+var newLib = new myLibrarry();
+
+console.log(" "  = newLib.?());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
