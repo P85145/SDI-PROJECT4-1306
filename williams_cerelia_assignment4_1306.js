e424dd97ac3190b6e485a62f8c33b3e665262247
P85145 <CeCe@Cerelias-MacBook-Pro.local>
2013-06-27 21:19:14 -0400

    Phone Function

--- williams_cerelia_assignment4_1306.js
+++ williams_cerelia_assignment4_1306.js
@@ -7,9 +7,29 @@
 
 
 
-var myLibrary = function(){
+var myLibrary = function() {
 
 	// FUNCTION
+	var checkPhone = function(val) {
+		
+		var strLen = val.length;				
+		for (var i=0; i <=strLen; i++){      
+		
+			if (val.substring(1, i+1) === "-") 
+			   (val.substring(1, i+4) === "-")
+			   (val.substring(1, i+8) === "-") 
+			
+				return true;
+				
+			} else {
+				return false;
+			}
+		
+		}
+		
+	} // end check string
+// change i=? in the function to start at a new position ex) looking for 2nd dash
+	
 	
 	
 	
@@ -39,36 +59,14 @@ var myLibrary = function(){
 
 	// Objects
 	return {
-		" ": ,
+		"checkPhone": checkPhone,
 		
 	}
 	
 }
 
-var newLib = new myLibrarry();
+var newLib = new myLibrary();
 
-console.log(" "  = newLib.?());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("Is the dash in the correct position? " + newLib.checkPhone("123-456-7890"));
 
 
